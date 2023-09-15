b9f811da14c1af2c4ac6ff5403de6f860f8e15a1
Manuel-Pincay <e1313614495@live.uleam.edu.ec>
2023-09-14 22:54:16 -0500

    mankar/app

---
+++ frontend-mankar/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from './routes'; // Importa tu archivo de rutas
+
+function App() {
+  return (
+    <div className="App">
+      <Routes /> {/* Renderiza las rutas */}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend-mankar/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import WelcomeHome from './view/home/home';
+import LoginForm from './view/login/LoginForm';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <App />
+);
