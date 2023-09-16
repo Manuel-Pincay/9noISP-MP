0f9a4a4f52c571734f46ea8b02f5972da652cf2e
Manuel-Pincay <e1313614495@live.uleam.edu.ec>
2023-09-16 00:47:49 -0500

    errores

--- frontend-mankar-angular/src/app/app.module.ts
+++ frontend-mankar-angular/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './Modulos/welcome/welcome.component';
 import { RouterModule, Routes } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { WelcomeComponent } from './Modulos/welcome/welcome.component';
 import { LoginAppComponent } from './Modulos/login-app/login-app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 const routes:Routes=[
   {path: '', redirectTo: 'inicio', pathMatch: 'full' },
   {path: 'inicio', component: WelcomeComponent, pathMatch: 'full'  },
-/*   {path: 'inicio-sesion', component: InicioSesionComponent, pathMatch: 'full'  },
+  {path: 'inicio-sesion', component: LoginAppComponent, pathMatch: 'full'  },/*
   {path: 'principal/:User', component: PrincipalComponent, pathMatch: 'full'}
  */
 ]
@@ -18,13 +31,19 @@ const routes:Routes=[
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    LoginAppComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    FormsModule
   ],
   exports:[RouterModule],
   providers: [],
