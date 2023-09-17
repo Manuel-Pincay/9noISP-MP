b6ffe50e7ee057f1cb980500c5d646fc07e39215
Manuel-Pincay <e1313614495@live.uleam.edu.ec>
2023-09-17 09:00:58 -0500

    areglado error

--- frontend-mankar-angular/src/app/app.module.ts
+++ frontend-mankar-angular/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -15,26 +17,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 import { WelcomeComponent } from './Modulos/welcome/welcome.component';
-import { LoginAppComponent } from './Modulos/login-app/login-app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PrincipalComponent } from './Modulos/principal/principal.component';
+import { InicioSesionComponent } from './Modulos/inicio-sesion/inicio-sesion.component';
 
 
 
 const routes:Routes=[
   {path: '', redirectTo: 'inicio', pathMatch: 'full' },
   {path: 'inicio', component: WelcomeComponent, pathMatch: 'full'  },
-  {path: 'inicio-sesion', component: LoginAppComponent, pathMatch: 'full'  },/*
-  {path: 'principal/:User', component: PrincipalComponent, pathMatch: 'full'}
- */
-  {path: 'principal', component: PrincipalComponent, pathMatch: 'full'}
+  {path: 'inicio-sesion', component: InicioSesionComponent, pathMatch: 'full'  },
+  {path: 'principal/:User', component: PrincipalComponent, pathMatch: 'full'},
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    InicioSesionComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +47,8 @@ const routes:Routes=[
     MatIconModule,
     MatFormFieldModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports:[RouterModule],
   providers: [],
